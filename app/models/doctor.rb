8ef72ee8501179f75bd2d667e364a3bcbcb128f6
Arique <arique.aguilar@gmail.com>
2020-09-22 10:32:31 -0500

    Models in application

---
+++ app/models/doctor.rb
@@ -0,0 +1,5 @@
+class Doctor < ApplicationRecord
+  validates_presence_of :name, :specialty, :university
+
+  belongs_to :hospital
+end
---
+++ app/models/surgery.rb
@@ -0,0 +1,7 @@
+class Surgery < ApplicationRecord
+ #attr_reader
+  #def initialize(*arg)
+
+  #end
+
+end
