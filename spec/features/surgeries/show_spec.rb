44207931976a22bd513be920aa915a5a8328e60d
Arique <arique.aguilar@gmail.com>
2020-09-22 11:54:44 -0500

    Clean up

--- spec/features/surgeries/show_spec.rb
+++ spec/features/surgeries/show_spec.rb
@@ -1,9 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "" do
-  # When I visit the surgery index page
-# I can click on any surgery title to take me to that surgery’s show page
-# And on the show page I see the title and operating room number of that surgery
 
   it "When I visit the surgery index page. I can click on any surgery title to take me to that surgery’s show page. And on the show page I see the title and operating room number of that surgery" do
     @hospital = Hospital.create!(name: "Arique Clinical Care")
@@ -29,8 +26,6 @@ RSpec.describe "" do
 
   end
 
-# And I see a section of the page that says "Other surgeries happening this day of the week:"
-# And under that header I see titles of all surgeries that happen on the same day of the week as this surgery.
 
   it "And I see a section of the page that says Other surgeries happening this day of the week: And under that header I see titles of all surgeries that happen on the same day of the week as this surgery." do
     @hospital = Hospital.create!(name: "Arique Clinical Care")
@@ -39,17 +34,12 @@ RSpec.describe "" do
     @doctor_2 = @hospital.doctors.create!(name: "Hillary", specialty: "Stent Specialist", university: "Carleton College" )
 
     @surgery_1 = Surgery.create!(title: "Aortal Heart Surgery", week_day: "Monday", operating_room: "N204")
-
-    #EXPECT SURGERY 2 TO NOT BE PRESENT
     @surgery_2 = Surgery.create!(title: "Open Heart Surgery", week_day: "Tuesday", operating_room: "W319")
-
     @surgery_3 = Surgery.create!(title: "Stent Heart Surgery", week_day: "Monday", operating_room: "N204")
     @surgery_4 = Surgery.create!(title: "Balloon Heart Surgery", week_day: "Monday", operating_room: "N204")
 
 
     DoctorSurgery.create!(doctor: @doctor_1, surgery: @surgery_1)
-
-    #EXPECT THESE TWO NOT TO BE PRESENT
     DoctorSurgery.create!(doctor: @doctor_1, surgery: @surgery_2)
     DoctorSurgery.create!(doctor: @doctor_2, surgery: @surgery_2)
     DoctorSurgery.create!(doctor: @doctor_2, surgery: @surgery_3)
@@ -69,10 +59,9 @@ RSpec.describe "" do
 
     within "#surgery-#{@surgery_1.id}" do
       expect(page).to have_content("Other surgeries happening this day of the week:")
+      expect(page).not_to have_content(@surgery_2.title)
       expect(page).to have_content(@surgery_3.title)
       expect(page).to have_content(@surgery_4.title)
     end
-    #add more surgeries that are on the same day!
-    # this will be an active model method through the joins table. Through surgeries and weekday attribute
   end
 end
