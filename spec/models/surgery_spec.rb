cbe086aad8e77684757c5451343e168fe5d49f4b
Arique <arique.aguilar@gmail.com>
2020-09-22 10:53:23 -0500

    Model tests for joins table

--- spec/models/surgery_spec.rb
+++ spec/models/surgery_spec.rb
@@ -1,1 +1,15 @@
 require 'rails_helper'
+
+RSpec.describe Surgery, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :title}
+    it {should validate_presence_of :week_day}
+    it {should validate_presence_of :operating_room}
+
+  end
+
+  describe "relationships" do
+    it {should have_many :doctor_surgeries}
+    it {should have_many(:doctors).through(:doctor_surgeries)}
+  end
+end
